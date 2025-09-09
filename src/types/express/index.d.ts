641366d02b1b1264015f73ef46c23aa80be26d79
Abhamid3311 <www.abdulhamid3311@gmail.com>
2025-09-09 15:48:15 +0600

    Added some new

--- src/types/express/index.d.ts
+++ src/types/express/index.d.ts
@@ -1,9 +1,12 @@
-import { IUser } from "src/app/modules/user/user.interface";
+// types/express/index.d.ts
+import { IUser } from "../../src/app/modules/user/user.interface";
 
 declare global {
   namespace Express {
     interface Request {
-      user?: IUser; // optional, added by your auth middleware
+      user?: IUser;
     }
   }
 }
+
+export {};
