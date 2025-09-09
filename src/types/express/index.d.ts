671546f19f0d11030120e17ec76cbc753e7518d2
Abhamid3311 <www.abdulhamid3311@gmail.com>
2025-09-09 15:54:31 +0600

    Solve

--- src/types/express/index.d.ts
+++ src/types/express/index.d.ts
@@ -1,16 +1,15 @@
-// types/express/index.d.ts
+import { Request } from "express";
 
-// Import the user interface
-import { IUser } from "../../src/app/modules/user/user.interface";
-
-// Extend Express namespace globally
 declare global {
   namespace Express {
     interface Request {
-      user?: IUser;
+      user?: {
+        id?: string;
+        email: string;
+        role?: string;
+      };
     }
   }
 }
 
-// Important: This makes it a module declaration, not a module replacement
 export {};
