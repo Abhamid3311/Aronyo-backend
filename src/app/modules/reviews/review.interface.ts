acef77f0d02bfde2a4eb67b1e7d47866116ba181
Abhamid3311 <www.abdulhamid3311@gmail.com>
2025-09-11 22:12:43 +0600

    Updated Review interface

--- src/app/modules/reviews/review.interface.ts
+++ src/app/modules/reviews/review.interface.ts
@@ -1,9 +1,9 @@
 import { Types } from "mongoose";
 
 export interface IReview {
-  userId: Types.ObjectId; // Reference to User
-  productId: Types.ObjectId; // Reference to Product
-  rating: number; // e.g. 1 to 5
+  userId: Types.ObjectId;
+  productId: Types.ObjectId;
+  rating: number;
   comment?: string;
   createdAt?: Date;
   updatedAt?: Date;
